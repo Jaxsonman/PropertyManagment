582537ef4d8febe2b8d85e9d23b6a9e1077d0bf0
Jaxsonman <42976152+Jaxsonman@users.noreply.github.com>
2018-10-23 16:34:38 -0600

    handled signUp post request

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -4,13 +4,15 @@ import { ROOT_URL } from '../config';
 
 export function signUp(fields, success) {
     console.log(fields);
+    success()
     return function(dispatch) {
-        // axios.post(`${ROOT_URL}/signUp`, fields)
-        //     .then(Response => {
-
-        //     })
-        //     .catch(err => {
-        //         if(err) { console.log(err)}
-        //     })
+        axios.post(`${ROOT_URL}/signUp`, fields)
+            .then(Response => {
+                console.log(response);
+                success()
+            })
+            .catch(err => {
+                if(err) { console.log(err)}
+            })
     }
 }
