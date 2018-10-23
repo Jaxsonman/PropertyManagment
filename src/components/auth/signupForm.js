25fc369685aae6b166156f3c9ebdb6acd44bec23
Jaxsonman <42976152+Jaxsonman@users.noreply.github.com>
2018-10-23 11:23:30 -0600

    built signup component

---
+++ src/components/auth/signupForm.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import { reduxForm, Field} from 'redux-form';
+import FormTitle from '../formTitle';
+import {FormInput, FormButton} from '../formFields';
+import TextLink from '../Textlink';
+
+
+class SignupForm extends Component {
+    render() {
+        return (
+            <form className='sign-up-form'>
+                <FormTitle className='sign-up-form__title' text='New User' />
+                <Field 
+                className='sign-up-form__fullname' 
+                placeholder='Enter Fullname' 
+                name='fullname' 
+                type='fullname'
+                title="Fullname"
+                component={FormInput} 
+                />
+                <Field 
+                className='sign-up-form__unit' 
+                placeholder='Enter Unit #' 
+                name='unit' 
+                type='unit'
+                title="Unit #"
+                component={FormInput} 
+                />
+                <Field 
+                className='sign-up-form__email' 
+                placeholder='Enter Email' 
+                name='email' 
+                type='email'
+                title="Email"
+                component={FormInput} 
+                />
+                <Field 
+                className='sign-up-form__password' 
+                placeholder='Enter Password' 
+                name='password' 
+                type='password'
+                title="Password"
+                component={FormInput} 
+                />
+                <Field 
+                className='sign-up-form__create-account' 
+                name='createaccount' 
+                type='submit'
+                title="Create Account"
+                component={FormButton} 
+                />
+               <div className="sign-up-form__text-links">
+                <TextLink to='/signin' text='Already Registered? Login'/>
+               </div>
+
+            </form>
+        )
+    }
+}
+
+SignupForm = reduxForm({
+    form: 'signup'
+})(SignupForm);
+
+export default SignupForm
