a7f9c2e180296ce3cbd5db349f746a51ea65883f
Jaxsonman <42976152+Jaxsonman@users.noreply.github.com>
2018-10-24 15:20:22 -0600

    built tabnav structure

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
+import Dashboard from './components/dashboard';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 
@@ -16,18 +17,25 @@ import Layout from './components/layout';
 import Signup from './components/auth/signup';
 import Signin from './components/auth/signin';
 
+import history from './history';
+import requireAuth from './components/requireAuth';
+
+
+
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-      <BrowserRouter>
+      <Router history={history}>
         <Switch>
           <Layout>
             <Route path='/' exact component={Signin}/>
             <Route path='/signin' component={Signin}/>
             <Route path='/signup' component={Signup}/>
+
+            <Route path='/dashboard' component={requireAuth(Dashboard)} />
           </Layout>
         </Switch>
-      </BrowserRouter>
+      </Router>
     </Provider>
     , document.querySelector('.app-wrapper'));
 }
