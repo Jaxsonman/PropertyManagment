f491d5ba892f61aa8c3e6d2a6dec1b49089a364d
Jaxsonman <42976152+Jaxsonman@users.noreply.github.com>
2018-10-25 10:32:12 -0600

    created newsletterArchvie and set grid

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -1,13 +1,15 @@
 import React,  {Component } from 'react';
 
 import NewsletterBox from './newsletterBox';
+import NewsletterArchive from'./newsletterArchive';
+import ArchiveItem from './newsletterArchive';
 
 class NewsletterGrid extends Component {
     render() {
         return (
             <div className='newsletter-grid'>
                 <NewsletterBox date={new Date()}/>
-                {/*  */}
+                <ArchiveItem />
             </div>
         )
     }
