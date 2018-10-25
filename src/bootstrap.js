6aad9e0855b3bb8b5747bc2e309ca39804af88a8
Jaxsonman <42976152+Jaxsonman@users.noreply.github.com>
2018-10-25 13:56:08 -0600

    plus button finished

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -5,22 +5,24 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { Router, Route, Switch } from 'react-router-dom';
 import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
-import Dashboard from './components/dashboard';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 
 // import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
 
+import history from './history';
+
 import Layout from './components/layout';
 
+// AUTH
+import requireAuth from './components/requireAuth';
 import Signup from './components/auth/signup';
 import Signin from './components/auth/signin';
 
-import history from './history';
-import requireAuth from './components/requireAuth';
-
-
+// DASHBOARD
+import Dashboard from './components/dashboard';
+import NewNewsletter from './components/newsletter/newsletterNew';
 
 function main() {
   ReactDOM.render(
@@ -32,7 +34,8 @@ function main() {
             <Route path='/signin' component={Signin}/>
             <Route path='/signup' component={Signup}/>
 
-            <Route path='/dashboard' component={requireAuth(Dashboard)} />
+            <Route path='/dashboard' component={requireAuth(Dashboard)}/>
+            <Route path='/newsletter/new' component={requireAuth(NewNewsletter)}/>
           </Layout>
         </Switch>
       </Router>
